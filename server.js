88c60bf70e42588aa252ca74d253bb1b943aab33
Leoul Zerihun <leoulzerihun06@gmail.com>
2026-09-05 18:43:03 +0300

    add first project

---
+++ server.js
@@ -0,0 +1,38 @@
+const http = require('http')
+const fs = require('fs')
+const server = http.createServer((req, res)=>{
+    let path='./views/'
+    switch (req.url){
+        case '/':
+            path+='index.html'
+            break
+        case '/about':
+            path+= 'about.html'
+            break;
+        case '/contact-me':
+            path+='contact-me.html'
+            break;
+        default:
+            path +='404.html';
+            break;
+    }
+    res.setHeader('content-Type', 'text/html')
+
+    fs.readFile(path, (err, data)=>{
+        if (err){
+            console.log(err);
+        }else{
+            res.write(data)
+            res.end()
+        }
+        
+    })
+
+    
+});
+server.listen(8080, 'localhost', ()=>{
+    console.log("Listening on 8080")
+})
+
+
+
